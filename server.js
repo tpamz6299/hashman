a188fc5e7a07f145fa57ad7680a7d7279420981a
tpamz6299 <tpamz6299@gmail.com>
2025-10-08 18:39:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+// Serve files from /public
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`✅ Server running on http://localhost:${PORT}`);
 });
